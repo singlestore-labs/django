528873b1b008b2cf35b443d250b3eb94db005e7f
Pavlo Mishchenko <pmishchenko-ua@singlestore.com>
2025-03-13 17:01:35 +0200

    Fix update tests

--- tests/_utils/setup.sql
+++ tests/_utils/setup.sql
@@ -317,3 +317,13 @@ CREATE TABLE `model_forms_article_category` (
   KEY (`article_id`),
   KEY (`category_id`)
 );
+
+-- update
+CREATE TABLE `update_bar_m2m_foo` (
+  `bar_id` BIGINT NOT NULL,
+  `foo_id` BIGINT NOT NULL,
+  SHARD KEY (`bar_id`),
+  UNIQUE KEY (`bar_id`, `foo_id`),
+  KEY (`bar_id`),
+  KEY (`foo_id`)
+);
