0a20f6388ccd6535ac26be903c8f49d2b13d68d2
Pavlo Mishchenko <pmishchenko-ua@singlestore.com>
2025-04-02 15:37:24 +0300

    Refactor tests

--- tests/_utils/local_test.sh
+++ tests/_utils/local_test.sh
@@ -2,47 +2,21 @@ export DJANGO_HOME=`pwd`
 
 export PYTHONPATH=$DJANGO_HOME:$DJANGO_HOME/tests:$DJANGO_HOME/tests/singlestore_settings:$PYTHONPATH
 
+# uncomment this to run tests without unique constraints on the data base level 
 # export NOT_ENFORCED_UNIQUE=1
 
-export TABLE_STORAGE_TYPE_ADMIN="ROWSTORE REFERENCE"
-export TABLE_STORAGE_TYPE_AUTH="ROWSTORE REFERENCE"
-export TABLE_STORAGE_TYPE_CONTENTTYPES="ROWSTORE REFERENCE"
-export TABLE_STORAGE_TYPE_SITES="ROWSTORE REFERENCE"
+# these are default django apps, we use ROWSTORE REFERENCE tabkes for them
+export  DJANGO_SINGLESTORE_TABLE_STORAGE_TYPE_ADMIN="ROWSTORE REFERENCE"
+export  DJANGO_SINGLESTORE_TABLE_STORAGE_TYPE_AUTH="ROWSTORE REFERENCE"
+export  DJANGO_SINGLESTORE_TABLE_STORAGE_TYPE_CONTENTTYPES="ROWSTORE REFERENCE"
+export  DJANGO_SINGLESTORE_TABLE_STORAGE_TYPE_SITES="ROWSTORE REFERENCE"
 
-
-export TABLE_STORAGE_TYPE_SERIALIZERS="ROWSTORE"
-export TABLE_STORAGE_TYPE_ADMIN_INLINES="ROWSTORE REFERENCE"
-export TABLE_STORAGE_TYPE_AUTH_TESTS="REFERENCE"
-export TABLE_STORAGE_TYPE_INTROSPECTION="ROWSTORE REFERENCE"
-export TABLE_STORAGE_TYPE_VALIDATION="ROWSTORE REFERENCE"
-export TABLE_STORAGE_TYPE_CONSTRAINTS="ROWSTORE REFERENCE"
-export TABLE_STORAGE_TYPE_BULK_CREATE="ROWSTORE REFERENCE"
-
-# 12 many-to-many fields
+# 12 many-to-many fields, just use reference tables to save time
 export TABLE_STORAGE_TYPE_PREFETCH_RELATED="ROWSTORE REFERENCE"
 
 # abstract models - specifying through is tricky 
 export TABLE_STORAGE_TYPE_MANY_TO_MANY="ROWSTORE REFERENCE"
 
-
-
-prepare_settings() {
-    for dir in $(find tests -maxdepth 1 -type d | awk -F '/' '{print $2}'); do
-        # echo $dir
-        sed -e "s|TEST_MODULE|$dir|g" tests/singlestore_settings_TMPL > tests/singlestore_settings/singlestore_settings_$dir.py
-    done
-}
-
-# prepare_settings
-
-
-run_all_tests() {
-    for dir in $(find tests -maxdepth 1 -type d | awk -F '/' '{print $2}'); do
-        echo $dir
-        ./tests/runtests.py --settings=singlestore_settings_$dir --noinput -v 3 $dir >> django_test_result_$dir.txt 2>&1
-    done
-}
-# python run_tests_parallel.py
-# run_all_tests
-
-./tests/runtests.py --settings=singlestore_settings --noinput -v 3 queries --keepdb
+# specify the path to the test to run
+MODULE_TO_TEST=serializers
+./tests/runtests.py --settings=singlestore_settings --noinput -v 3 $MODULE_TO_TEST --keepdb
