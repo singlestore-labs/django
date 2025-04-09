c412ef52de49360599be648a94ee39db8220aafe
Parth Sharma <psharma@singlestore.com>
2025-04-09 16:02:36 +0530

    fix of update_only_field test

--- tests/_utils/setup.sql
+++ tests/_utils/setup.sql
@@ -409,3 +409,13 @@ CREATE TABLE `serializers_m2mintermediatedata_anchor` (
   KEY (`m2mintermediatedata_id`),
   KEY (`anchor_id`)
 );
+
+--update_only_fields
+CREATE TABLE `update_only_fields_employee_account` (
+  `employee_id` BIGINT NOT NULL,
+  `account_id` BIGINT NOT NULL,
+  SHARD KEY (`employee_id`),
+  UNIQUE KEY (`employee_id`, `account_id`),
+  KEY (`employee_id`),
+  KEY (`account_id`)
+);
