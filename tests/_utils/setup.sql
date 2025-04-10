eb517fd40384a9504910376213fb1648515845ca
Pavlo Mishchenko <pmishchenko-ua@singlestore.com>
2025-04-10 15:43:51 +0300

    Change models for contenttypes_tests

--- tests/_utils/setup.sql
+++ tests/_utils/setup.sql
@@ -419,3 +419,13 @@ CREATE TABLE `update_only_fields_employee_account` (
   KEY (`employee_id`),
   KEY (`account_id`)
 );
+
+-- contenttypes_tests
+CREATE TABLE `contenttypes_tests_modelwithm2mtosite_site` (
+  `modelwithm2mtosite_id` BIGINT NOT NULL,
+  `site_id` BIGINT NOT NULL,
+  SHARD KEY (`modelwithm2mtosite_id`),
+  UNIQUE KEY (`modelwithm2mtosite_id`, `site_id`),
+  KEY (`modelwithm2mtosite_id`),
+  KEY (`site_id`)
+);
