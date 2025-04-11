1e179463a52439d717a5ce76582411fc9f7c328a
psharma-1909 <psharma@singlestore.com>
2025-04-11 17:28:58 +0530

    Change model for test_runner tests (#7)
    * fix of test_runner tests
    
    * nit: change in related_name value

--- tests/_utils/setup.sql
+++ tests/_utils/setup.sql
@@ -429,3 +429,13 @@ CREATE TABLE `contenttypes_tests_modelwithm2mtosite_site` (
   KEY (`modelwithm2mtosite_id`),
   KEY (`site_id`)
 );
+
+--test_runner
+CREATE TABLE `test_runner_person_friend` (
+  `from_person_id` BIGINT NOT NULL,
+  `to_person_id` BIGINT NOT NULL,
+  SHARD KEY (`from_person_id`),
+  UNIQUE KEY (`from_person_id`, `to_person_id`),
+  KEY (`from_person_id`),
+  KEY (`to_person_id`)
+);
