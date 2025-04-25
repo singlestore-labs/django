446d913bd5ee0fad8a04869c82a3a468a3973439
Pavlo Mishchenko <65538066+pmishchenko-ua@users.noreply.github.com>
2025-04-25 11:54:28 +0300

    Fix multiple_database tests (#11)

--- tests/_utils/setup.sql
+++ tests/_utils/setup.sql
@@ -458,3 +458,13 @@ CREATE TABLE `model_fields_allfieldsmodel_allfieldsmodel` (
   KEY (`from_allfieldsmodel_id`),
   KEY (`to_allfieldsmodel_id`)
 );
+
+-- multiple_database
+CREATE TABLE `multiple_database_book_person` (
+  `book_id` BIGINT NOT NULL,
+  `person_id` VARCHAR(100) NOT NULL,
+  SHARD KEY (`book_id`),
+  UNIQUE KEY (`book_id`, `person_id`),
+  KEY (`book_id`),
+  KEY (`person_id`)
+);
