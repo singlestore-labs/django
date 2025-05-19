243a91404036df7d184e2bc73e62cf8003aa2e76
Pavlo Mishchenko <pmishchenko-ua@singlestore.com>
2025-05-19 15:57:02 +0300

    Change models in one_to_one tests

--- tests/_utils/setup.sql
+++ tests/_utils/setup.sql
@@ -551,3 +551,13 @@ CREATE TABLE `fixtures_naturalkeything_naturalkeything` (
   KEY (`from_naturalkeything_id`),
   KEY (`to_naturalkeything_id`)
 );
+
+-- one_to_one
+CREATE TABLE `one_to_one_favorites_restaurant` (
+  `favorites_id` BIGINT NOT NULL,
+  `restaurant_id` BIGINT NOT NULL,
+  SHARD KEY (`favorites_id`),
+  UNIQUE KEY (`favorites_id`, `restaurant_id`),
+  KEY (`favorites_id`),
+  KEY (`restaurant_id`)
+);
