f5cdcb2e73f4dd94dcab424d905bc06b0ec7f28f
psharma-1909 <psharma@singlestore.com>
2025-06-02 16:34:22 +0530

    Fix of db_functions tests (#23)
    * Fix of db_functions test
    
    * change migration (#24)
    
    * Changes in test
    
    * reverted the changes for Concat test
    
    * added the deleted unsigned ints
    
    ---------
    
    Co-authored-by: Pavlo Mishchenko <65538066+pmishchenko-ua@users.noreply.github.com>

--- tests/_utils/setup.sql
+++ tests/_utils/setup.sql
@@ -600,3 +600,13 @@ CREATE TABLE `delete_player_game` (
   KEY (`player_id`),
   KEY(`game_id`)
 );
+
+-- db_functions
+CREATE TABLE `db_functions_article_author` (
+  `article_id` BIGINT NOT NULL,
+  `author_id` BIGINT NOT NULL,
+  SHARD KEY (`article_id`),
+  UNIQUE KEY (`article_id`, `author_id`),
+  KEY (`article_id`),
+  KEY (`author_id`)
+);
