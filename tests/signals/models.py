69406ced0d607bd5a8bc267fed0e20836cdeb45f
Parth Sharma <psharma@singlestore.com>
2025-07-30 18:54:01 +0530

    Fix of signals

--- tests/signals/models.py
+++ tests/signals/models.py
@@ -26,10 +26,19 @@ class Author(models.Model):
 
 class Book(models.Model):
     name = models.CharField(max_length=20)
-    authors = models.ManyToManyField(Author)
+    authors = models.ManyToManyField("Author", through="BookAuthor")
 
     def __str__(self):
         return self.name
+
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('book', 'author'),)
+        db_table = "signals_book_author"
 
 
 class Page(models.Model):
