c7d3bb5fb1b2b559d0fd0912c1ccddcf2510f812
psharma-1909 <psharma@singlestore.com>
2025-07-31 22:04:54 +0530

    Psharma/fix tests group1a (#40)
    * Fix custom_column tests
    
    * Fix datetimes tests

--- tests/custom_columns/tests.py
+++ tests/custom_columns/tests.py
@@ -34,7 +34,7 @@ class CustomColumnsTests(TestCase):
     def test_field_error(self):
         msg = (
             "Cannot resolve keyword 'firstname' into field. Choices are: "
-            "Author_ID, article, first_name, last_name, primary_set"
+            "Author_ID, article, articleauthor, first_name, last_name, primary_set"
         )
         with self.assertRaisesMessage(FieldError, msg):
             Author.objects.filter(firstname__exact="John")
@@ -81,7 +81,7 @@ class CustomColumnsTests(TestCase):
     def test_filter_on_nonexistent_field(self):
         msg = (
             "Cannot resolve keyword 'firstname' into field. Choices are: "
-            "Author_ID, article, first_name, last_name, primary_set"
+            "Author_ID, article, articleauthor, first_name, last_name, primary_set"
         )
         with self.assertRaisesMessage(FieldError, msg):
             Author.objects.filter(firstname__exact="John")
