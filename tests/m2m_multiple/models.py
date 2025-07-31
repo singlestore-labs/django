7bdc3ca03c10ab24fcd8407dfbe2acd9c9d87644
psharma-1909 <psharma@singlestore.com>
2025-07-31 22:03:35 +0530

    Psharma/fix tests group0a (#41)
    * Fix of model_formsets_regress tests
    
    * Fix of filtered_relation tests
    
    * Fix of admin_filters tests
    
    * Fix of managers_regress
    
    * Fix of signals
    
    * Fix of migrations tests
    
    * Fix of m2m_multiple tests

--- tests/m2m_multiple/models.py
+++ tests/m2m_multiple/models.py
@@ -24,10 +24,10 @@ class Article(models.Model):
     headline = models.CharField(max_length=50)
     pub_date = models.DateTimeField()
     primary_categories = models.ManyToManyField(
-        Category, related_name="primary_article_set"
+        Category, related_name="primary_article_set", through="ArticleCategory"
     )
     secondary_categories = models.ManyToManyField(
-        Category, related_name="secondary_article_set"
+        Category, related_name="secondary_article_set", through="ArticleCategoryS"
     )
 
     class Meta:
@@ -35,3 +35,20 @@ class Article(models.Model):
 
     def __str__(self):
         return self.headline
+
+class ArticleCategory(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('article', 'category'),)
+        db_table = "m2m_multiple_article_category"
+
+
+class ArticleCategoryS(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('article', 'category'),)
+        db_table = "m2m_multiple_article_category_second"
